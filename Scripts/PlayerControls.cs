1a41457c5153268db87928db50b58ed9eef59e50
MorriganDM <pedrovicthorcordeiro@gmail.com>
2025-02-26 20:15:12 -0300

    Update PlayerControls.cs
    Added torque to rotate left.

--- Scripts/PlayerControls.cs
+++ Scripts/PlayerControls.cs
@@ -1,16 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rb2d;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+             rb2d.AddTorque(torqueAmount);
+        }
     }
 }
